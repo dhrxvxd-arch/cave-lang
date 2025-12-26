45a8f9a4beac377e193c675bce1c81745677ad49
dhrxvxd <dhrxvxd@google.com>
2025-12-26 21:02:32 +0530

    Stupid c: Uninitialized char array

--- src/cave.c
+++ src/cave.c
@@ -25,28 +25,24 @@ struct command_data
     struct str_optional output;
 };
 
-void readbuf(const char *path, char *buf, size_t *size)
+void readbuf(const char *path, char **buf, size_t *size)
 {
     FILE *file = fopen(path, "rb");
     if (!file)
-    {
-        perror("Failed to open file");
         return;
-    }
 
     fseek(file, 0, SEEK_END);
     *size = ftell(file);
     rewind(file);
 
-    buf = (char *)malloc(*size);
-    if (!buf)
+    *buf = malloc(*size);
+    if (!*buf)
     {
-        perror("Failed to allocate memory");
         fclose(file);
         return;
     }
 
-    fread(buf, 1, *size, file);
+    fread(*buf, 1, *size, file);
     fclose(file);
 }
 
@@ -54,96 +50,89 @@ bool check_flags(char *argv[], int argc)
 {
     bool has_flag = false;
     bool has_input = false;
+
     for (int i = 1; i < argc; i++)
     {
-        for (char *p = argv[i]; *p != '\0'; p++)
+        if (argv[i][0] == '-')
         {
-            if (*p == '-')
-            {
-                has_flag = true;
-                if (*(p + 1) == 'i')
-                {
-                    has_input = true;
-                }
-                break;
-            }
+            has_flag = true;
+            if (strchr(argv[i], 'i'))
+                has_input = true;
         }
     }
+
     return has_flag && has_input;
 }
 
 struct command_data parse_flags(char *argv[], int argc)
 {
-    struct command_data data;
-    data.verbose = false;
+    struct command_data data = {0};
     data.output.state = ABSENT;
 
     for (int i = 1; i < argc; i++)
     {
-        for (char *p = argv[i]; *p != '\0'; p++)
+        if (argv[i][0] != '-')
+            continue;
+
+        char *p = argv[i] + 1;
+        while (*p)
         {
-            if (*p == '-')
+            switch (*p)
             {
-                p++;
-                switch (*p)
+            case 'v':
+                data.verbose = true;
+                break;
+            case 'o':
+                if (*(p + 1))
                 {
-                case 'v':
-                    data.verbose = true;
-                    break;
-                case 'o':
-                    if (*(p + 1) == '\0' && (i + 1) < argc)
-                    {
-                        data.output.str = argv[i + 1];
-                        data.output.state = PRESENT;
-                        i++;
-                    }
-                    else if (*(p + 1) != '\0')
-                    {
-                        data.output.str = p + 1;
-                        data.output.state = PRESENT;
-                    }
-                    break;
-                case 'i':
-                    if (*(p + 1) == '\0' && (i + 1) < argc)
-                    {
-                        data.input_path = argv[i + 1];
-                        i++;
-                    }
-                    else if (*(p + 1) != '\0')
-                    {
-                        data.input_path = p + 1;
-                    }
-                    break;
-                default:
-                    break;
+                    data.output.str = p + 1;
+                    data.output.state = PRESENT;
+                    p += strlen(p + 1);
                 }
+                else if (i + 1 < argc)
+                {
+                    data.output.str = argv[++i];
+                    data.output.state = PRESENT;
+                }
+                break;
+            case 'i':
+                if (*(p + 1))
+                {
+                    data.input_path = p + 1;
+                    p += strlen(p + 1);
+                }
+                else if (i + 1 < argc)
+                {
+                    data.input_path = argv[++i];
+                }
+                break;
+            default:
+                break;
             }
+            p++;
         }
     }
+
+    return data;
 }
 
 int main(int argc, char *argv[])
 {
-    char file_path[PATH_MAX];
-    char *buffer;
-    size_t size;
+    char *buffer = NULL;
+    size_t size = 0;
 
     if (argc < 2)
-    {
-        fprintf(stderr, "Usage: %s -i <file_path>\n", argv[0]);
         return EXIT_FAILURE;
-    }
 
     if (!check_flags(argv, argc))
-    {
-        fprintf(stderr, "Invalid flags provided.\n");
         return EXIT_FAILURE;
-    }
 
-    struct command_data data;
-    parse_flags(argv, argc);
+    struct command_data data = parse_flags(argv, argc);
+    if (!data.input_path)
+        return EXIT_FAILURE;
 
-    file_path[PATH_MAX - 1] = '\0';
-    readbuf(file_path, buffer, &size);
+    readbuf(data.input_path, &buffer, &size);
+
+    free(buffer);
     return EXIT_SUCCESS;
 }
